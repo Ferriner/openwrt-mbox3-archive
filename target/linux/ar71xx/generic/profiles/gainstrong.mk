3db27169dc3150717735207201b96b7ee49221bc
Ferriner <arknort@gmail.com>
2017-06-07 10:32:19 +0400

    Update gainstrong.mk

--- target/linux/ar71xx/generic/profiles/gainstrong.mk
+++ target/linux/ar71xx/generic/profiles/gainstrong.mk
@@ -26,3 +26,15 @@ define Profile/OOLITE/Description
 	Package set optimized for the Gainstrong OOLITE.
 endef
 $(eval $(call Profile,OOLITE))
+
+
+define Profile/MINIBOXV3
+	NAME:=Gainstrong MiniBox V3
+	PACKAGES:=kmod-usb-core kmod-usb2  kmod-ledtrig-usbdev
+endef
+
+
+define Profile/MINIBOXV3/Description
+	Package set optimized for the Gainstrong MiniBox V3
+endef
+$(eval $(call Profile,MINIBOXV3))
