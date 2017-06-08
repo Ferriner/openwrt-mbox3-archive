ca9ba72ad564111c598b5efe3d78799201d65674
Ferriner <arknort@gmail.com>
2017-06-08 11:28:46 +0400

    Create mach-gs-minibox-v3.c

--- target/linux/ar71xx/files/arch/mips/ath79/mach-gs-minibox-v3.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-gs-minibox-v3.c
@@ -8,13 +8,14 @@
  */
 
 #include <linux/gpio.h>
+#include <linux/platform_device.h>
+#include <linux/ar8216_platform.h>
 
-#include <asm/mach-ath79/ath79.h>
 #include <asm/mach-ath79/ar71xx_regs.h>
 
 #include "common.h"
-#include "dev-eth.h"
 #include "dev-gpio-buttons.h"
+#include "dev-eth.h"
 #include "dev-leds-gpio.h"
 #include "dev-m25p80.h"
 #include "dev-usb.h"
@@ -22,11 +23,12 @@
 #include "machtypes.h"
 
 #define GS_MINIBOX_V3_GPIO_BTN_RESET	17
-
 #define GS_MINIBOX_V3_GPIO_LED_SYSTEM	14
 
 #define GS_MINIBOX_V3_KEYS_POLL_INTERVAL	20	/* msecs */
 #define GS_MINIBOX_V3_KEYS_DEBOUNCE_INTERVAL (3 * GS_MINIBOX_V3_KEYS_POLL_INTERVAL)
+
+#define GS_MINIBOX_V3_WMAC_CALDATA_OFFSET	0x1000
 
 static const char *gs_minibox_v3_part_probes[] = {
 	"tp-link",
@@ -59,26 +61,41 @@ static struct gpio_keys_button gs_minibox_v3_gpio_keys[] __initdata = {
 static void __init gs_minibox_v3_setup(void)
 {
 	u8 *mac = (u8 *) KSEG1ADDR(0x1f01fc00);
-	u8 *ee = (u8 *) KSEG1ADDR(0x1fff1000);
+	u8 *art = (u8 *) KSEG1ADDR(0x1fff0000);
 
+	ath79_setup_ar933x_phy4_switch(false, false);
+	
 	ath79_register_leds_gpio(-1, ARRAY_SIZE(gs_minibox_v3_leds_gpio),
 				 gs_minibox_v3_leds_gpio);
 
 	ath79_register_gpio_keys_polled(-1, GS_MINIBOX_V3_KEYS_POLL_INTERVAL,
 					ARRAY_SIZE(gs_minibox_v3_gpio_keys),
 					gs_minibox_v3_gpio_keys);
-
+	
+	ath79_register_m25p80(&gs_minibox_v3_flash_data);
 	ath79_register_usb();
 
-	ath79_register_m25p80(&gs_minibox_v3_flash_data);
-	ath79_init_mac(ath79_eth0_data.mac_addr, mac, 1);
-	ath79_init_mac(ath79_eth1_data.mac_addr, mac, -1);
-
 	ath79_register_mdio(0, 0x0);
-	ath79_register_eth(1);
+	
+	/* WAN */
+	ath79_eth0_data.duplex = DUPLEX_FULL;
+	ath79_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_MII;
+	ath79_eth0_data.speed = SPEED_100;
+	ath79_eth0_data.phy_mask = BIT(4);
+	ath79_init_mac(ath79_eth0_data.mac_addr, mac, 1);
 	ath79_register_eth(0);
 
-	ath79_register_wmac(ee, mac);
+	/* LAN */
+	ath79_switch_data.phy4_mii_en = 1;
+	ath79_eth1_data.duplex = DUPLEX_FULL;
+	ath79_eth1_data.phy_if_mode = PHY_INTERFACE_MODE_GMII;
+	ath79_eth1_data.speed = SPEED_1000;
+	ath79_switch_data.phy_poll_mask |= BIT(4);
+	ath79_init_mac(ath79_eth1_data.mac_addr, mac, -1);
+	ath79_register_eth(1);
+
+	ath79_register_wmac(art + GS_MINIBOX_V3_WMAC_CALDATA_OFFSET, mac);
+
 }
 
 MIPS_MACHINE(ATH79_MACH_GS_MINIBOX_V3, "MINIBOX-V3",
