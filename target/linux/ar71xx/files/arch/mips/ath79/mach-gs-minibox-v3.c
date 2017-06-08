6b12a9518f64f87c6a49af7f3cdc0a45abbee606
Ferriner <arknort@gmail.com>
2017-06-08 12:01:15 +0400

    Update mach-gs-minibox-v3.c

--- target/linux/ar71xx/files/arch/mips/ath79/mach-gs-minibox-v3.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-gs-minibox-v3.c
@@ -1,10 +1,21 @@
 /*
- *  Gainstrong MiniBox V3 board support
+ * TP-LINK TL-WR810N board support
  *
+ * Copyright (c) 2012 Qualcomm Atheros
+ * Copyright (c) 2012 Gabor Juhos <juhosg@openwrt.org>
+ * Copyright (c) 2016 Jens Steinhauser <jens.steinhauser@gmail.com>
  *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License version 2 as published
- *  by the Free Software Foundation.
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include <linux/gpio.h>
@@ -22,61 +33,79 @@
 #include "dev-wmac.h"
 #include "machtypes.h"
 
-#define GS_MINIBOX_V3_GPIO_BTN_RESET	17
-#define GS_MINIBOX_V3_GPIO_LED_SYSTEM	14
+#define TL_WR810N_GPIO_SWITCH_B1	0
+#define TL_WR810N_GPIO_SWITCH_B0	1
+#define TL_WR810N_GPIO_USB_POWER	11
+#define TL_WR810N_GPIO_BTN_RESET	12
+#define TL_WR810N_GPIO_LED_SYSTEM	13
 
-#define GS_MINIBOX_V3_KEYS_POLL_INTERVAL	20	/* msecs */
-#define GS_MINIBOX_V3_KEYS_DEBOUNCE_INTERVAL (3 * GS_MINIBOX_V3_KEYS_POLL_INTERVAL)
+#define TL_WR810N_KEYS_POLL_INTERVAL	20 /* msecs */
+#define TL_WR810N_KEYS_DEBOUNCE_INTERVAL (3 * TL_WR810N_KEYS_POLL_INTERVAL)
 
-#define GS_MINIBOX_V3_WMAC_CALDATA_OFFSET	0x1000
+#define TL_WR810N_WMAC_CALDATA_OFFSET	0x1000
 
-static const char *gs_minibox_v3_part_probes[] = {
+static const char *tl_wr810n_part_probes[] = {
 	"tp-link",
 	NULL,
 };
 
-static struct flash_platform_data gs_minibox_v3_flash_data = {
-	.part_probes	= gs_minibox_v3_part_probes,
+static struct flash_platform_data tl_wr810n_flash_data = {
+	.part_probes = tl_wr810n_part_probes,
 };
 
-static struct gpio_led gs_minibox_v3_leds_gpio[] __initdata = {
+static struct gpio_led tl_wr810n_leds_gpio[] __initdata = {
 	{
-		.name		= "minibox-v3:green:system",
-		.gpio		= GS_MINIBOX_V3_GPIO_LED_SYSTEM,
+		.name		= "tp-link:blue:system",
+		.gpio		= TL_WR810N_GPIO_LED_SYSTEM,
 		.active_low	= 1,
 	},
 };
 
-static struct gpio_keys_button gs_minibox_v3_gpio_keys[] __initdata = {
+static struct gpio_keys_button tl_wr810n_gpio_keys[] __initdata = {
 	{
 		.desc		= "reset",
 		.type		= EV_KEY,
 		.code		= KEY_RESTART,
-		.debounce_interval = GS_MINIBOX_V3_KEYS_DEBOUNCE_INTERVAL,
-		.gpio		= GS_MINIBOX_V3_GPIO_BTN_RESET,
+		.debounce_interval = TL_WR810N_KEYS_DEBOUNCE_INTERVAL,
+		.gpio		= TL_WR810N_GPIO_BTN_RESET,
+		.active_low	= 1,
+	},
+	{
+		.desc		= "switch_b0",
+		.type		= EV_SW,
+		.code		= BTN_0,
+		.debounce_interval = TL_WR810N_KEYS_DEBOUNCE_INTERVAL,
+		.gpio		= TL_WR810N_GPIO_SWITCH_B0,
+		.active_low	= 0,
+	},
+	{
+		.desc		= "switch_b1",
+		.type		= EV_SW,
+		.code		= BTN_1,
+		.debounce_interval = TL_WR810N_KEYS_DEBOUNCE_INTERVAL,
+		.gpio		= TL_WR810N_GPIO_SWITCH_B1,
 		.active_low	= 0,
 	},
 };
 
-static void __init gs_minibox_v3_setup(void)
+static void __init tl_wr810n_setup(void)
 {
 	u8 *mac = (u8 *) KSEG1ADDR(0x1f01fc00);
 	u8 *art = (u8 *) KSEG1ADDR(0x1fff0000);
 
 	ath79_setup_ar933x_phy4_switch(false, false);
-	
-	ath79_register_leds_gpio(-1, ARRAY_SIZE(gs_minibox_v3_leds_gpio),
-				 gs_minibox_v3_leds_gpio);
 
-	ath79_register_gpio_keys_polled(-1, GS_MINIBOX_V3_KEYS_POLL_INTERVAL,
-					ARRAY_SIZE(gs_minibox_v3_gpio_keys),
-					gs_minibox_v3_gpio_keys);
-	
-	ath79_register_m25p80(&gs_minibox_v3_flash_data);
-	ath79_register_usb();
+	ath79_register_m25p80(&tl_wr810n_flash_data);
+	ath79_register_leds_gpio(-1,
+				 ARRAY_SIZE(tl_wr810n_leds_gpio),
+				 tl_wr810n_leds_gpio);
+	ath79_register_gpio_keys_polled(-1,
+				        TL_WR810N_KEYS_POLL_INTERVAL,
+				        ARRAY_SIZE(tl_wr810n_gpio_keys),
+				        tl_wr810n_gpio_keys);
 
 	ath79_register_mdio(0, 0x0);
-	
+
 	/* WAN */
 	ath79_eth0_data.duplex = DUPLEX_FULL;
 	ath79_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_MII;
@@ -94,9 +123,13 @@ static void __init gs_minibox_v3_setup(void)
 	ath79_init_mac(ath79_eth1_data.mac_addr, mac, -1);
 	ath79_register_eth(1);
 
-	ath79_register_wmac(art + GS_MINIBOX_V3_WMAC_CALDATA_OFFSET, mac);
+	ath79_register_wmac(art + TL_WR810N_WMAC_CALDATA_OFFSET, mac);
 
+	gpio_request_one(TL_WR810N_GPIO_USB_POWER,
+			 GPIOF_OUT_INIT_HIGH | GPIOF_EXPORT_DIR_FIXED,
+			 "USB power");
+	ath79_register_usb();
 }
 
-MIPS_MACHINE(ATH79_MACH_GS_MINIBOX_V3, "MINIBOX-V3",
-	     "MiniBox V3", gs_minibox_v3_setup);
+MIPS_MACHINE(ATH79_MACH_TL_WR810N, "TL-WR810N", "TP-LINK TL-WR810N",
+	     tl_wr810n_setup);
